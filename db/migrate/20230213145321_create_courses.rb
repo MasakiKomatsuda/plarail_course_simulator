6c8883e08a090d5ff93e0ef54018f73b7fe28ef1
komatsudamasaki <masakichiomatu@gmail.com>
2023-02-14 00:11:06 +0900

    add: courseテーブルの作成

--- db/migrate/20230213145321_create_courses.rb
+++ db/migrate/20230213145321_create_courses.rb
@@ -2,8 +2,8 @@ class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :title
-      t.text :description
+      t.string :title, null: false
+      t.text :description, null: false
       t.string :image
 
       t.timestamps
