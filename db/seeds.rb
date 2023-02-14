db1dd7d93a8a71c697680fc47d69bb943000ba2e
komatsudamasaki <masakichiomatu@gmail.com>
2023-02-14 23:39:58 +0900

    add: seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  User.create!(
+      name: Faker::Name.unique.name,
+      email: Faker::Internet.unique.email,
+      password: '12345678',
+      password_confirmation: '12345678',
+      avatar: Faker::Avatar.unique.image
+  )
+end
+
+20.times do |index|
+  Course.create!(
+      user: User.offset(rand(User.count)).first,
+      title: "タイトル#{index}",
+      description: "説明#{index}",
+      image: Faker::LoremFlickr.image
+  )
+end
